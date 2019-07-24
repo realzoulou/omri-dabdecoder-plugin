b34cb45a1deb4a9556b76ee7a2d8cbc125c487b9
realzoulou <zoulourns@gmail.com>
2019-07-24 20:48:23 +0200

    fix: dlopen failed: cannot locate symbol "__aeabi_memcpy" referenced by "[...]libmpg123plug.so"

--- dabmpg123plugin/jni/Application.mk
+++ dabmpg123plugin/jni/Application.mk
@@ -1,3 +1,5 @@
 APP_STL := c++_shared
-APP_PLATFORM := android-28
+# NDK version definition is omitted here because it depends on minSdkVersion
+# Use gradle to define APP_PLATFORM (called ANDROID_PLATFORM there)
+# APP_PLATFORM := android-<minSdkVersion>
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
