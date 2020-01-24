27cda1b94cb3cd6239183c8a525cb3c06c0cae02
realzoulou <zoulourns@gmail.com>
2020-01-24 20:25:02 +0100

    APP_PLATFORM=android-19
    was 16 before because of minSdkVersion in build.gradle

--- dabmpg123plugin/jni/Application.mk
+++ dabmpg123plugin/jni/Application.mk
@@ -1,5 +1,3 @@
 APP_STL := c++_shared
-# NDK version definition is omitted here because it depends on minSdkVersion
-# Use gradle to define APP_PLATFORM (called ANDROID_PLATFORM there)
-# APP_PLATFORM := android-<minSdkVersion>
+APP_PLATFORM := android-19
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
